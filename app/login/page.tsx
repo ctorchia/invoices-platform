f879cb54b3eec9200f4559ab76d0075c9b165e57
ctorchia <cristian_torchia@yahoo.com.ar>
2024-12-27 13:25:37 -0300

    Primer Commit

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div>
+            <h1>Hello from the Login route</h1>
+        </div>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <div>
+
+      <h1>Hello from the index page</h1>
+
+    </div>
+    
+  );
+}
