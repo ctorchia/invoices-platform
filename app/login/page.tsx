37dbcb5933466da3b47cd97a7d37e821ab8adbde
ctorchia <cristian_torchia@yahoo.com.ar>
2025-01-11 18:16:07 -0300

    Logo en Public

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,14 +5,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { auth, signIn } from "../utils/auth";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SubmitButton } from "../components/SubmitButtons";
-import { signIn } from "../utils/auth";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+    const session = await auth();
+
+    if (session?.user) {
+        redirect("/dashboard");
+    }
   return (
     <>
       <div className="flex h-screen w-full items-center justify-center px-4">
