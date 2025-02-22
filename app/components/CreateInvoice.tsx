4c50268cda798e52ea71d6984ba03e8fa4cbe129
ctorchia <cristian_torchia@yahoo.com.ar>
2025-02-21 21:29:52 -0300

    4:41:00

--- app/components/CreateInvoice.tsx
+++ app/components/CreateInvoice.tsx
@@ -24,157 +24,181 @@ import { Label } from "@/components/ui/label";
 import { SubmitButton } from "./SubmitButtons";
 import { Textarea } from "@/components/ui/textarea";
 import { createInvoice } from "../actions";
+import { invoiceSchema } from "../utils/zodSchemas";
+import { parseWithZod } from "@conform-to/zod";
+import { useForm } from "@conform-to/react";
 
 export function CreateInvoice() {
-  const[lastResult, action] = useActionState(createInvoice, undefined);
+  const [lastResult, action] = useActionState(createInvoice, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: invoiceSchema,
+      });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
 
   const [selectedDate, setSelectedDate] = useState(new Date());
   return (
     <Card className="w-full max-w-4xl mx-auto">
       <CardContent className="p-6">
-        <div className="flex flex-col gap-1 w-fit mb-6">
-          <div className="flex items-center gap-4">
-            <Badge variant="secondary">Draft</Badge>
-            <Input placeholder="Test 123" />
+        <form id={form.id} action={action} onSubmit={form.onSubmit} noValidate>
+          <div className="flex flex-col gap-1 w-fit mb-6">
+            <div className="flex items-center gap-4">
+              <Badge variant="secondary">Draft</Badge>
+              <Input
+                name={fields.invoiceName.name}
+                key={fields.invoiceName.key}
+                defaultValue={fields.invoiceName.initialValue}
+                placeholder="Test 123"
+              />
+            </div>
+            <p className="text-sm text-red-500">{fields.invoiceName.errors}</p>
           </div>
-        </div>
 
-        <div className="grid md:grid-cols-3 gap-6 mb-6">
-          <div>
-            <Label>Invoice No.</Label>
-            <div className="flex">
-              <span className="px-3 border border-r-0 rounded-l-md bg-muted flex items-center">
-                #
-              </span>
-              <Input className="rounded-l-none" placeholder="5" />
-            </div>
-          </div>
-          <div>
-            <Label>Currency</Label>
-            <Select defaultValue="USD">
-              <SelectTrigger>
-                <SelectValue placeholder="Select Currency" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="USD">United States Dollar -- USD</SelectItem>
-                <SelectItem value="EUR">Euro -- EUR</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-        <div className="grid md:grid-cols-2 gap-6 mb-6">
-          <div>
-            <Label>From:</Label>
-            <div className="space-y-2">
-              <Input placeholder="Your Name" />
-              <Input placeholder="Your Email" />
-              <Input placeholder="Your Address" />
-            </div>
-          </div>
-          <div>
-            <Label>To:</Label>
-            <div className="space-y-2">
-              <Input placeholder="Client Name" />
-              <Input placeholder="Client Email" />
-              <Input placeholder="Client Address" />
-            </div>
-          </div>
-        </div>
-        <div className="grid md:grid-cols-2 gap-6 mb-6">
-          <div>
+          <div className="grid md:grid-cols-3 gap-6 mb-6">
             <div>
-              <Label>Date</Label>
+              <Label>Invoice No.</Label>
+              <div className="flex">
+                <span className="px-3 border border-r-0 rounded-l-md bg-muted flex items-center">
+                  #
+                </span>
+                <Input className="rounded-l-none" placeholder="5" />
+              </div>
             </div>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="outline"
-                  className="w-[280px] text-left justify-start"
-                >
-                  <CalendarIcon />
-                  {selectedDate ? (
-                    new Intl.DateTimeFormat("es-ES", {
-                      //"en-US"
-                      dateStyle: "long",
-                    }).format(selectedDate)
-                  ) : (
-                    <span>Pick a Date</span>
-                  )}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent>
-                <Calendar
-                  selected={selectedDate}
-                  onSelect={(date) => setSelectedDate(date || new Date())}
-                  mode="single"
-                  fromDate={new Date()}
-                />
-              </PopoverContent>
-            </Popover>
+            <div>
+              <Label>Currency</Label>
+              <Select defaultValue="USD">
+                <SelectTrigger>
+                  <SelectValue placeholder="Select Currency" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="USD">
+                    United States Dollar -- USD
+                  </SelectItem>
+                  <SelectItem value="EUR">Euro -- EUR</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
+          <div className="grid md:grid-cols-2 gap-6 mb-6">
+            <div>
+              <Label>From:</Label>
+              <div className="space-y-2">
+                <Input placeholder="Your Name" />
+                <Input placeholder="Your Email" />
+                <Input placeholder="Your Address" />
+              </div>
+            </div>
+            <div>
+              <Label>To:</Label>
+              <div className="space-y-2">
+                <Input placeholder="Client Name" />
+                <Input placeholder="Client Email" />
+                <Input placeholder="Client Address" />
+              </div>
+            </div>
+          </div>
+          <div className="grid md:grid-cols-2 gap-6 mb-6">
+            <div>
+              <div>
+                <Label>Date</Label>
+              </div>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant="outline"
+                    className="w-[280px] text-left justify-start"
+                  >
+                    <CalendarIcon />
+                    {selectedDate ? (
+                      new Intl.DateTimeFormat("es-ES", {
+                        //"en-US"
+                        dateStyle: "long",
+                      }).format(selectedDate)
+                    ) : (
+                      <span>Pick a Date</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <Calendar
+                    selected={selectedDate}
+                    onSelect={(date) => setSelectedDate(date || new Date())}
+                    mode="single"
+                    fromDate={new Date()}
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+            <div>
+              <Label>Invoice Due:</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select Due Date" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="0">Due on Receipt</SelectItem>
+                  <SelectItem value="15">Net 15</SelectItem>
+                  <SelectItem value="30">Net 30</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+
           <div>
-            <Label>Invoice Due:</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Select Due Date" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="0">Due on Receipt</SelectItem>
-                <SelectItem value="15">Net 15</SelectItem>
-                <SelectItem value="30">Net 30</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-
-        <div>
-          <div className="grid grid-cols-12 gap-4 mb-2 font-medium">
-            <p className="col-span-6">Description:</p>
-            <p className="col-span-2">Quantity:</p>
-            <p className="col-span-2">Rate:</p>
-            <p className="col-span-2">Amount:</p>
-          </div>
-          <div className="grid grid-cols-12 gap-4 mb-4">
-            <div className="col-span-6">
-              <Textarea placeholder="Item Name & Description" />
+            <div className="grid grid-cols-12 gap-4 mb-2 font-medium">
+              <p className="col-span-6">Description:</p>
+              <p className="col-span-2">Quantity:</p>
+              <p className="col-span-2">Rate:</p>
+              <p className="col-span-2">Amount:</p>
             </div>
-            <div className="col-span-2">
-              <Input type="number" placeholder="0" />
-            </div>
-            <div className="col-span-2">
-              <Input type="number" placeholder="0" />
-            </div>
-            <div className="col-span-2">
-              <Input type="number" placeholder="0" disabled />
+            <div className="grid grid-cols-12 gap-4 mb-4">
+              <div className="col-span-6">
+                <Textarea placeholder="Item Name & Description" />
+              </div>
+              <div className="col-span-2">
+                <Input type="number" placeholder="0" />
+              </div>
+              <div className="col-span-2">
+                <Input type="number" placeholder="0" />
+              </div>
+              <div className="col-span-2">
+                <Input type="number" placeholder="0" disabled />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex justify-end">
-          <div className="w-1/3">
-            <div className="flex justify-between py-2">
-              <span>Subtotal:</span>
-              <span>$5.00</span>
-            </div>
-            <div className="flex justify-between py-2 border-t">
-              <span>Total: (USD)</span>
-              <span className="font-medium underline underline-offset-2">
-                $5.00
-              </span>
+          <div className="flex justify-end">
+            <div className="w-1/3">
+              <div className="flex justify-between py-2">
+                <span>Subtotal:</span>
+                <span>$5.00</span>
+              </div>
+              <div className="flex justify-between py-2 border-t">
+                <span>Total: (USD)</span>
+                <span className="font-medium underline underline-offset-2">
+                  $5.00
+                </span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div>
-          <Label>Note</Label>
-          <Textarea placeholder="Add your Note/s right here..." />
-        </div>
-
-        <div className="flex items-center justify-end mt-6">
           <div>
-            <SubmitButton text="Send Invoice to Client" />
+            <Label>Note</Label>
+            <Textarea placeholder="Add your Note/s right here..." />
           </div>
-        </div>
+
+          <div className="flex items-center justify-end mt-6">
+            <div>
+              <SubmitButton text="Send Invoice to Client" />
+            </div>
+          </div>
+        </form>
       </CardContent>
     </Card>
   );
