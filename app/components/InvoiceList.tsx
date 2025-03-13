9ff6683a228b3b8a1d523b037b356f93c284879f
ctorchia <cristian_torchia@yahoo.com.ar>
2025-03-12 22:18:54 -0300

    9:53:00 (Suspense)

--- app/components/InvoiceList.tsx
+++ app/components/InvoiceList.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/table";
 
 import { Badge } from "@/components/ui/badge";
+import { EmptyState } from "./EmptyState";
 import { InvoiceActions } from "./InvoiceActions";
 import { formatCurrency } from "../utils/formatCurrency";
 import prisma from "../utils/db";
@@ -34,45 +35,57 @@ async function getData(userId: string) {
 
   return data;
 }
-
 export async function InvoiceList() {
   const session = await requireUser();
   const data = await getData(session.user?.id as string);
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Invoice ID</TableHead>
-          <TableHead>Customer</TableHead>
-          <TableHead>Amount</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Date</TableHead>
-          <TableHead className="text-right">Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map((invoice) => (
-          <TableRow key={invoice.id}>
-            <TableCell>#{invoice.invoiceNumber}</TableCell>
-            <TableCell>{invoice.clientName}</TableCell>
-            <TableCell>
-              {formatCurrency({
-                amount: invoice.total,
-                currency: invoice.currency as any,
-              })}
-            </TableCell>
-            <TableCell><Badge>{invoice.status}</Badge></TableCell>
-            <TableCell>
-              {new Intl.DateTimeFormat("es-US", {
-                dateStyle: "medium",
-              }).format(invoice.createdAt)}
-            </TableCell>
-            <TableCell className="text-right">
-            <InvoiceActions status={invoice.status} id={invoice.id} />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      {data.length === 0 ? (
+        <EmptyState
+          title="No invoices found"
+          description="Create an invoice to get started"
+          buttontext="Create invoice"
+          href="/dashboard/invoices/create"
+        />
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Invoice ID</TableHead>
+              <TableHead>Customer</TableHead>
+              <TableHead>Amount</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((invoice) => (
+              <TableRow key={invoice.id}>
+                <TableCell>#{invoice.invoiceNumber}</TableCell>
+                <TableCell>{invoice.clientName}</TableCell>
+                <TableCell>
+                  {formatCurrency({
+                    amount: invoice.total,
+                    currency: invoice.currency as any,
+                  })}
+                </TableCell>
+                <TableCell>
+                  <Badge>{invoice.status}</Badge>
+                </TableCell>
+                <TableCell>
+                  {new Intl.DateTimeFormat("en-US", {
+                    dateStyle: "medium",
+                  }).format(invoice.createdAt)}
+                </TableCell>
+                <TableCell className="text-right">
+                  <InvoiceActions status={invoice.status} id={invoice.id} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </>
   );
 }
